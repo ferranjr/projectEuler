d215ff817bdb37f70f121f657f6701b04314e5ec
Ferran Puig Calvache <ferran.jr@gmail.com>
2016-03-31 14:23:03 +0100

    puzzle steps

--- src/main/scala/com/ferranjr/Algorithms.scala
+++ src/main/scala/com/ferranjr/Algorithms.scala
@@ -5,7 +5,7 @@ import scala.collection.immutable.HashSet
 object Algorithms {
 
   /**
-   * Schibsted test
+   * Schibsted Algorithm test
    * ~~~~~~
    * Find in a constant time, if there exists two pairs into an array that add the given number
    *
@@ -27,4 +27,29 @@ object Algorithms {
     }
     false
   }
+
+  /**
+   * Schibsted Puzzle
+   * ~~~~~~
+   * Given 25 horses, having had them racing 5 times, how many more races we will need to
+   * know which are the 3 top horses knowing
+   *
+   *
+   *   a1 > a2 > a3 > a4 > a5
+   *   b1 > b2 > b3 > b4 > b5
+   *   c1 > c2 > c3 > c4 > c5
+   *   d1 > d2 > d3 > d4 > d5
+   *   e1 > e2 > e3 > e4 > e5
+   *
+   * STEP 1 .
+   *   Run all the first ones... which will allow us to discard lots of them
+   * STEP 2 .
+   *   Discard all the candidates that for sure are slower than the top 3
+   * STEP 3 .
+   *   Run the remaining
+   *
+   * It should be 7 races to get the right answer
+   *
+   *
+   */
 }
